0dd4c40a474f71603557b623570a2b060402c823
Harish Malviya <144028633+harishmlv@users.noreply.github.com>
2024-03-09 16:55:11 +0530

    Update MainPage.js

--- multiple_Page_routing2/src/pages/MainPage.js
+++ multiple_Page_routing2/src/pages/MainPage.js
@@ -2,14 +2,16 @@ import React from "react";
 import Home from "./Home";
 import About from "./About";
 import Contact from "./Contact";
-// import Home from "./Home";
-// import About from "./About";
-// import Contact from "./Contact";
 import {Link,Route,Routes} from "react-router-dom";
 
-/* link = used to define hyperlink of web pages
-   route = used to define the path of web pages
-   routes = Collection of route. */
+/* - path = The path prop defines the route path.
+   - element = The component prop defines the React component to render when the route path matches the current URL.
+   - route = used to define the path of web pages
+   - routes = Collection of route. The <Routes> component is used to define the different routes that are available in your application. 
+      Each individual route is defined using a <Route> component.
+   - link = used to define hyperlink of web pages. Now we will use the <Link> component of react-router-dom to 
+      create clickable links that change the URL of the browser and render a different component based on the new URL. 
+*/
 
    function MainPage()
    {
